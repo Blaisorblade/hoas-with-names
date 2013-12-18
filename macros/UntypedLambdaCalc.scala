953a2f82973dd739ddf16862ab72371e64727020
Paolo Giarrusso <p.giarrusso@gmail.com>
2013-12-18 18:45:02 +0100

    Fix whitespace

--- macros/UntypedLambdaCalc.scala
+++ macros/UntypedLambdaCalc.scala
@@ -4,13 +4,16 @@ trait UntypedLambdaCalc {
   //should only be used for cosmetic purposes, such as making pretty-printing
   //results more readable.
   sealed trait Term
+
   case class Lambda(name: String, userSpecified: Boolean, hoasBody: Term => Term) extends Term {
     override def toString = {
       case class Var(name: String) extends Term
       s"Lambda($name => ${hoasBody(Var(name))})"
     }
   }
+
   case class Apply(fun: Term, arg: Term)
+
   //TODO: convert to some other first-order representation for pattern-matching. Consider using deBrujin terms, following for instance
   //"Unembedding domain-specific languages".
 }
