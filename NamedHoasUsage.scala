5f9c40d043016384e9d71103577246ef7a959f1f
Paolo Giarrusso <p.giarrusso@gmail.com>
2013-12-18 18:45:22 +0100

    More usage examples

--- NamedHoasUsage.scala
+++ NamedHoasUsage.scala
@@ -3,6 +3,11 @@ object NamedHoasUsage extends App {
   println(lambda(x => x))
   println(lambda(identity)) //Test freshname generation.
   println(lambda(identity)) //Do it again.
+  println(lambda(identity(x => x)))
+  //identity compose identity doesn't work.
+  //println(lambda(identity compose identity)) //Do it again.
+  val id = identity[Term] _
+  println(lambda(id compose id)) //Do it again.
   println(lambda(x => lambda(y => x)))
   println(lambda(x => lambda(y => y)))
 }
