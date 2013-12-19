84b094d581432f56ec58b1f2b29b03f2219da121
Paolo Giarrusso <p.giarrusso@gmail.com>
2013-12-19 16:01:31 +0100

    Distinguish names from strings
    This will allow to disambiguate names using indexes without embedding
    the indexes in the names.

--- macros/UntypedLambdaCalc.scala
+++ macros/UntypedLambdaCalc.scala
@@ -1,14 +1,31 @@
-trait UntypedLambdaCalc {
+import scala.language.implicitConversions
+
+trait Names {
+  sealed trait Name {
+    def name: String
+  }
+  case class StringName(name: String) extends Name
+  case class IdxStringName(prefix: String, idx: Int) extends Name {
+    //It's a val to cache the result.
+    override val name = prefix + idx
+  }
+  implicit def string2name(name: String): Name = StringName(name)
+}
+
+trait UntypedLambdaCalc extends Names {
   //A HOAS-based term representation for lambda-calculus. Function terms carry
   //a name, but this is only to remember what name the user specified, and
   //should only be used for cosmetic purposes, such as making pretty-printing
   //results more readable.
   sealed trait Term
 
-  case class Lambda(name: String, userSpecified: Boolean, hoasBody: Term => Term) extends Term {
+  case class Lambda(name: Name, userSpecified: Boolean, hoasBody: Term => Term) extends Term {
+    //XXX Consider moving this to prettyprint.
     override def toString = {
-      case class Var(name: String) extends Term
-      s"Lambda($name => ${hoasBody(Var(name))})"
+      case class Var(name: Name) extends Term {
+        override def toString = name.name
+      }
+      s"Lambda(${name.name} => ${hoasBody(Var(name))})"
     }
   }
 
