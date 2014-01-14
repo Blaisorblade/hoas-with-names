af0067e983aaa5635f4dc47550e9c8212791cd2f
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-01-14 15:35:21 +0100

    Extract reusable getArgName

--- macros/Macros.scala
+++ macros/Macros.scala
@@ -4,6 +4,16 @@ object Macros extends UntypedLambdaCalc {
   def getSrcLoc[T](c: Context)(expr: c.Expr[T]) = {
   }
 
+  def getArgName[S, T](c: Context)(fun: c.Expr[S => T]): Option[String] = {
+    import c.universe._
+    fun match {
+      case Expr(Function(List(ValDef(mods, paramName, typ, _)), body)) =>
+        Some(paramName.decoded)
+      case _ =>
+        None
+    }
+  }
+
   //Let's have a macro returning Lambda. That is, Macros.Lambda, where Macros is the runtime singleton object Macros.
   def lambda(hoasBody: Term => Term): Lambda = macro lambda_impl
 
@@ -15,13 +25,10 @@ object Macros extends UntypedLambdaCalc {
 
   def lambda_impl(c: Context { type PrefixType = Macros.type })(hoasBody: c.Expr[Term => Term]): c.Expr[c.prefix.value.Lambda] = {
     import c.universe._
-    val (name, userSpecified) =
-      hoasBody match {
-        case Expr(Function(List(ValDef(mods, paramName, typ, _)), body)) =>
-          (paramName.decoded, true)
-        case _ =>
-          (c.fresh("x_"), false) //Reuse freshname generator from macros.
-      }
+    val (name, userSpecified) = getArgName(c)(hoasBody) match {
+      case Some(name) => (name, true)
+      case None => (c.fresh("x_"), false) //Reuse freshname generator from macros.
+    }
     reify(Lambda(c.literal(name).splice, c.literal(userSpecified).splice, hoasBody.splice))
   }
 }
